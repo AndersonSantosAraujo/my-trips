e8e9fca2b30081dfe002bd8ede03fccef9500adf
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2023-10-29 23:25:49 -0300

    feat: creating LinkWrapper component with TDD (#26)

---
+++ src/components/LinkWrapper/test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+import LinkWrapper from '.'
+
+describe('<linkWrapper/>', () => {
+  it('should render link and children', () => {
+    render(<LinkWrapper href="/my-link">Anything</LinkWrapper>)
+
+    const children = screen.getByRole('link', { name: /anything/i })
+
+    expect(children).toBeInTheDocument()
+    expect(children).toHaveAttribute('href', '/my-link')
+
+    // screen.logTestingPlaygroundURL()
+  })
+})
