d7050104aeedce1e048a669c50625a1ef92efc4d
Anderson Araujo <anderson.dos.santos.araujo@hotmail.com>
2023-10-31 03:16:26 -0300

    feat: getStatic/getServerSide methods (#31)

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,36 @@
+import client from 'graphql/client'
+import { GET_PAGES } from 'graphql/queries'
 import AboutTemplate from 'templates/About'
 
-const About = () => {
+type Pages = {
+  pages: Page[]
+}
+
+type Page = {
+  id: string
+  heading: string
+  slug: string
+  body: {
+    html: HTMLElement
+  }
+}
+
+export default function AboutPage() {
   return <AboutTemplate />
 }
 
-export default About
+export const getStaticProps = async () => {
+  const { pages }: { pages: Pages } = await client.request(GET_PAGES)
+
+  console.log(pages)
+
+  return {
+    props: {}
+  }
+}
+
+// MÉTODOS DO NEXTJS
+// getStaticPaths -> gerar as URLS das páginas em build time
+// getStaticProps -> buscar dados da página (props) em build time
+// getServerSideProps -> buscar dados da páfina em runtime (toda request) o bundle fica no server
+// getInitialProps -> similar ao getServerSideProps, porém o bundle também no client (hydrate); está em desuso
